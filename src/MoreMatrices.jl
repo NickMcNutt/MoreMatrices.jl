379d2b74467c916764258e16f31a50be143afa40
Nick McNutt <nmcnutt@utk.edu>
2015-11-16 16:19:25 -0500

    Initial files

--- src/MoreMatrices.jl
+++ src/MoreMatrices.jl
@@ -1,18 +1,18 @@
 module MoreMatrices
 
 export
-    random_perturb!,
-    random_permute!,
-    random_rotate!,
-    block_matrix_R,
-    extract_rotations,
-    extract_rotations2,
-    apply_rotations,
-    kabsch,
+    # operations.jl
+
     kronshuffle!,
     kronshuffle,
-    kronfactor
+    kronfactor,
+
+    # random_matrices.jl
+
+    random_orthogonal,
+    random_permutation
 
 include("operations.jl")
+include("random_matrices.jl")
 
 end
